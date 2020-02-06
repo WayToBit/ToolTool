6449d5f6336d2fe09aec9ed84a9b494613f95cad
ecoming <byungmoon.kwak@gmail.com>
2020-02-06 15:09:37 +0900

    Create ChocolateyExample.ps1

---
+++ ChocolateyExample.ps1
@@ -0,0 +1,8 @@
+# Upgrade all installed packages to latest version.
+choco upgrade all -y
+
+# Install flavored packages
+choco install 7zip -y
+choco install googlechrome -y
+choco install gom-player -y
+
